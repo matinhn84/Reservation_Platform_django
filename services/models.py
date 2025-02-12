a777c40114d120d9b4ce729c5c757aa360b25e2c
Matin <mohamad13840208@gmail.com>
2025-02-12 13:55:21 +0330

    1.add media root in settings.py 2.define services/models.py

--- services/models.py
+++ services/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+from django.utils.translation import gettext_lazy as _
+
 # Create your models here.
+
+
+class Service(models.Model):
+    user = models.ForeignKey(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    img = models.ImageField(_("service image"), upload_to='services/', height_field=50, width_field=50, max_length=1)
+    title = models.TextField(_("service title"))
+    price = models.IntegerField(_("service price"))
+    duration = models.IntegerField(_("service duration"))
+    description = models.TextField(_("service description"))
